4be1a07d40039d3efe10621738d2b2c1d4e5da76
jmtristante <91247278+jmtristante@users.noreply.github.com>
2022-05-11 16:23:05 +0200

    Update deploy.sh

--- wazuh-kubernetes/scripts/deploy.sh
+++ wazuh-kubernetes/scripts/deploy.sh
@@ -3,7 +3,7 @@
 . ./data.sh
 echo $HELM_RELEASE_NAME
 
-helm install --name $HELM_RELEASE_NAME \
+helm install $HELM_RELEASE_NAME \
     --namespace $KUBE_NAMESPACE $HELM_CHART_PATH \
     -f $HELM_VALUES_PATH -f $HELM_METADATA -f $HELM_APPDATA \
     --set identifier.appId=${APP_ID} \
@@ -11,4 +11,4 @@ helm install --name $HELM_RELEASE_NAME \
     --set identifier.spaceId=${SPACE_ID} \
     --set identifier.spaceName=${SPACE_NAME} \
     --set identifier.clusterId=${CLUSTER_ID} \
-    --debug --no-hooks
+    --debug --no-hooks --disable-openapi-validation
