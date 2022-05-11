f3cab37cc9ddaa36d36f45ce25a7db81628670bc
jmtristante <91247278+jmtristante@users.noreply.github.com>
2022-05-11 16:22:23 +0200

    Update test.sh

--- wazuh-kubernetes/scripts/test.sh
+++ wazuh-kubernetes/scripts/test.sh
@@ -15,7 +15,7 @@ helm lint \
     --set identifier.clusterId=${CLUSTER_ID} \
 
 echo "Test Templating..."
-helm install --name $HELM_RELEASE_NAME \
+helm install $HELM_RELEASE_NAME \
     --namespace $KUBE_NAMESPACE $HELM_CHART_PATH \
     -f $HELM_VALUES_PATH -f $HELM_METADATA -f $HELM_APPDATA \
     --set identifier.appId=${APP_ID} \
@@ -23,4 +23,4 @@ helm install --name $HELM_RELEASE_NAME \
     --set identifier.spaceId=${SPACE_ID} \
     --set identifier.spaceName=${SPACE_NAME} \
     --set identifier.clusterId=${CLUSTER_ID} \
-    --debug --dry-run  --wait --no-hooks
+    --debug --dry-run  --wait --no-hooks --disable-openapi-validation
